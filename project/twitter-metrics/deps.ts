136949275f89e7f782c320fff99472254d7d0d6f
Michael J. Ryan <tracker1@gmail.com>
2022-09-27 18:07:08 -0700

    save wip, getting 409 error, may need to call stream directly

--- project/twitter-metrics/deps.ts
+++ project/twitter-metrics/deps.ts
@@ -1,8 +1,6 @@
 import * as path from "https://deno.land/std@0.157.0/path/mod.ts";
 import { configSync as dotenvConfig } from "https://deno.land/std@0.157.0/dotenv/mod.ts";
-// import TwitterClient from "https://deno.land/x/simple_twitter_deno@0.02/simple_twitter_deno.ts";
-import * as TwitterClient from "https://deno.land/x/twitter_api_client@v0.2.2/mod.ts";
 
 import { clone } from "https://raw.githubusercontent.com/tracker1/deno-lib/main/utils/clone.ts";
 
-export { clone, dotenvConfig, path, TwitterClient };
+export { clone, dotenvConfig, path };
--- project/twitter-metrics/server.ts
+++ project/twitter-metrics/server.ts
@@ -1,26 +1,22 @@
 #!/usr/bin/env -S deno run --allow-read --allow-env --allow-net
 
 import { load as loadEnv } from "./lib/dotenv.ts";
-import { clone, TwitterClient } from "./deps.ts";
+import {
+  connectStream as connectTwitterStream,
+  StreamTweet,
+} from "https://deno.land/x/twitter_api_client@v0.2.2/api_v2/tweets/filtered_stream.ts";
 
 loadEnv();
 
-const twitterConfig = {
-  consumer_key: Deno.env.get("CONSUMER_KEY"),
-  consumer_secret: Deno.env.get("CONSUMER_SECRET"),
-  access_token: Deno.env.get("ACCESS_TOKEN"),
-  access_token_secret: Deno.env.get("ACCESS_TOKEN_SECRET"),
-  bearer_token: Deno.env.get("BEARER_TOKEN"),
-};
+const BEARER_TOKEN = Deno.env.get("BEARER_TOKEN") || "";
 
-const twitter = new TwitterClient(twitterConfig);
-
-try {
-  const stream = twitter.stream("statuses/sample", undefined);
-} catch (err) {
-  console.log(clone(err));
+// deno-lint-ignore require-await
+async function handleTweet(tweet: StreamTweet) {
+  console.log({ tweet });
 }
 
-// stream?.on("tweet", function (tweet) {
-//   console.log(tweet);
-// });
+// none option
+const disconnect = connectTwitterStream(BEARER_TOKEN, handleTweet);
+
+// Disconnect Stream after 10sec.
+setTimeout(() => disconnect(), 10 * 1000);
